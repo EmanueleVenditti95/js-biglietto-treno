5d6aa742560214b1cb356c7ccc9678b80ca3b661
EmanueleVenditti95 <vendittiema@gmail.com>
2023-10-04 15:13:07 +0200

    Fix - readme

---
+++ app.js
@@ -0,0 +1,27 @@
+// # Calcolo del prezzo del biglietto del treno
+
+
+// - chiedere all'utente quanti km dovrà percorrere
+//     - creare una variabile (kmUser) con i km percorsi dall'utente
+//     - chiedere i km all'utente attraverso un prompt
+//     - assegnare alla variabile (kmUser) il valore restituito dal prompt
+
+// - chiedere all'utente l'età
+//     - creare una variabile (ageUSer) con l'età dell'utente
+//     - chiedere all'utente l'età attraverso un prompt
+//     - assegnare alla variabile (ageUser) il valore restituito dal prompt
+
+// - definire il prezzo
+//     - creare una variabile che definisca il prezzo del biglietto in base ai km 
+
+// - definire gli sconti
+//     - creare una o più variabili che definiscano gli sconti
+//         - creare una variabile per lo sconto di 20% (Junior)
+//         - creare una variabile per lo sconto di 40% (Senior)
+
+// - calcolare lo sconto
+//     - SE l'età dell'utente è < 18 anni, applicare lo sconto Junior
+//     - ALTRIMENTI SE l'età dell'utente è > 65 anni,applicare lo sconto Senior   
+//     - ALTRIMENTI non applicare nessuno sconto
+     
+// - emettere il prezzo finale
--- readme.md
+++ readme.md
@@ -1,12 +1,5 @@
 # Calcolo del prezzo del biglietto del treno
 
-- definire il prezzo base
-    - creare una variabile che definisca il prezzo del biglietto in base ai km 
-
-- definire gli sconti
-    - creare una o più variabili che definiscano gli sconti
-        - creare una variabile per lo sconto di 20% (Junior)
-        - creare una variabile per lo sconto di 40% (Senior)
 
 - chiedere all'utente quanti km dovrà percorrere
     - creare una variabile (kmUser) con i km percorsi dall'utente
@@ -17,6 +10,14 @@
     - creare una variabile (ageUSer) con l'età dell'utente
     - chiedere all'utente l'età attraverso un prompt
     - assegnare alla variabile (ageUser) il valore restituito dal prompt
+
+- definire il prezzo
+    - creare una variabile che definisca il prezzo del biglietto in base ai km 
+
+- definire gli sconti
+    - creare una o più variabili che definiscano gli sconti
+        - creare una variabile per lo sconto di 20% (Junior)
+        - creare una variabile per lo sconto di 40% (Senior)
 
 - calcolare lo sconto
     - SE l'età dell'utente è < 18 anni, applicare lo sconto Junior
